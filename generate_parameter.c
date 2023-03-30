6cfb7c49298c0a22d378c6bf0fae32a07f9ce793
sjj <sjj@sjj-debian>
2023-03-31 04:56:44 +0800

    增加generate_parameter程序的交互功能
    将generate_parameter程序的width设计为用户从命令行输入指定
    避免不同位宽需要重复编译

--- generate_parameter.c
+++ generate_parameter.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
+#include <malloc.h>
 
-#define width 32
+#define a(i,j) a[width*2*i+j]
 
 int main(void)
 {
-    int a[width/2+1][width*2]={0};
+	int width;
+	printf("Input Multer Width:");
+	scanf("%d", &width);
+	int *a = (int *)malloc((width/2+1)*(width*2)*sizeof(int));
     int i, j;
 
+    for(i=0; i<width/2+1; i++) {
+        for(j=0; j<width*2; j++) {
+            a(i,j) = 0;
+        }
+    }
     for(i=0; i<width/2; i++) {
-        for(j=i*2, a[i][j]+=1; j<(i+1)*2+width; j++) {
-            a[i][j]++;
+        for(j=i*2, a(i,j)+=1; j<(i+1)*2+width; j++) {
+            a(i,j)++;
         }
     }
     for(i=width; i<width*2; i++) {
-        a[width/2][i]++;
+        a(width/2,i)++;
     }
     for(i=1; i<width/2+1; i++) {
         for(j=0; j<width*2; j++) {
-            a[0][j] += a[i][j];
+            a(0,j) += a(i,j);
         }
     }
     for(i=0; i<width*2; i++)
-        printf("%d ", a[0][i]);
+        printf("%d ", a(0,i));
     printf("\n\n");
     return 0;
 }
